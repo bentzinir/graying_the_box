85f499856f111a6b926375a2434b99193ac0c4d2
Nir Ben-Zrihem <nirb@mobileye.com>
2016-02-29 16:34:05 +0200

    added hdf5 save

--- common.py
+++ common.py
@@ -5,3 +5,9 @@ def load_hdf5(object_name, path, num_frames):
     obj_file = h5py.File(path + object_name + '.h5', 'r')
     obj_mat = obj_file['data']
     return obj_mat[:num_frames]
+
+def save_hdf5(object_name, path, object):
+    print "    Saving " +  object_name
+    obj_file = h5py.File(path + object_name + '.h5', 'w')
+    obj_mat = obj_file.create_dataset(object_name, data=object)
+    obj_file.close()
